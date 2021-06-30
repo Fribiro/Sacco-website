9aeb612bed8e600a28aca1927e455ac1aa952573
Festus Ribiro <festusribiro@gmail.com>
2021-06-30 15:44:20 +0300

    Changed navbar code structure and basic structure of the contact page

--- src/components/Header.style.js
+++ src/components/Header.style.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom"
 import { Navbar, Nav, NavDropdown }from "react-bootstrap";
 
 const media = {
@@ -22,6 +23,11 @@ const Navwrapper = styled.div`
     display: none;
   }
 
+  .dropdown-menu {
+    background-color: #f8f9fa !important;
+    border: 0;
+  }
+
   & .navbar-dark .navbar-brand {
     padding: 0;
   }
@@ -30,14 +36,23 @@ const Navwrapper = styled.div`
     margin-left: 4rem;
   }
 
-  & .dropdown-menu.show {
-    margin-top: 2rem;
+  & .fCnFeG .dropdown-menu.show {
+    margin-top: 1rem !important;
+    border-top: 0;
     border-radius: 0;
+  }
+
+  & .dropdown:hover .dropdown-menu {
+    display: block;
+  }
+
+  & .navbar-dark .navbar-toggler-icon {
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
   }
 
   ${media.laptop} {
   }
-  & a.nav-link:hover {
+  /* & a.nav-link:hover {
     cursor: pointer;
     color: #76bf30;
   }
@@ -57,7 +72,7 @@ const Navwrapper = styled.div`
   & a.nav-link:hover::after {
     transform: scaleX(1);
     transform-origin: center;
-  }
+  } */
 `;
 
 const Header = () => {
@@ -65,16 +80,15 @@ const Header = () => {
   return (
     <div>
       <Navwrapper>
-        
-        <Navbar collapseOnSelect expand="lg" bg="white" variant="dark">
+        {/* <Navbar collapseOnSelect expand="lg" bg="white" variant="dark">
           <Navbar.Brand href="#home">
             <img
               src="uniqlogo.png"
               style={{ width: "6rem", height: "3.5rem" }}
             ></img>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
+          <Navbar.Toggle aria-controls="basic-navbar-nav navbar-light" />
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="about">About</Nav.Link>
@@ -118,7 +132,116 @@ const Header = () => {
               <Nav.Link href="contacts">Contacts</Nav.Link>
             </Nav>
           </Navbar.Collapse>
-        </Navbar>
+        </Navbar> */}
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <Link class="navbar-brand" href="#">
+            <img
+              src="uniqlogo.png"
+              style={{ width: "6rem", height: "3.5rem" }}
+            ></img>
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse"
+            id="navbarNavDropdown"
+          >
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <Link class="nav-link" to="/">
+                  Home <span class="sr-only">(current)</span>
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" href="#">
+                  Features
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" href="#">
+                  About
+                </Link>
+              </li>
+              <li class="nav-item dropdown">
+                <Link
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Savings Products
+                </Link>
+                <div
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <Link class="dropdown-item" href="#">
+                    Action
+                  </Link>
+                  <Link class="dropdown-item" href="#">
+                    Another action
+                  </Link>
+                  <Link class="dropdown-item" href="#">
+                    Something else here
+                  </Link>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <Link
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Loan Products
+                </Link>
+                <div
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <Link class="dropdown-item" href="#">
+                    Normal Loan
+                  </Link>
+                  <Link class="dropdown-item" href="#">
+                    Education Loan
+                  </Link>
+                  <Link class="dropdown-item" href="#">
+                    Asset Finance
+                  </Link>
+                  <Link class="dropdown-item" href="#">
+                    Development Loan
+                  </Link>
+                  <Link class="dropdown-item" href="#">
+                    Emergency Loan
+                  </Link>
+                </div>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" href="#">
+                  Loan Calculator
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="contacts">
+                  Contacts
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </Navwrapper>
     </div>
   );
--- src/components/contacts/ContactCards.style.js
+++ src/components/contacts/ContactCards.style.js
@@ -1,11 +1,241 @@
 import React from 'react'
+import styled from "styled-components";
+
+const media = {
+  mobile: "@media (max-width: 480px)",
+  tablet: "@media (min-width: 481px) and (max-width: 768px)",
+  laptop: "@media (min-width: 769px) and (max-width: 1024px)",
+  desktop: "@media (min-width: 1025px) and (max-width: 1200px)",
+  extralarge: "@media (min-width: 1201px)",
+};
+
+const Main = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 1rem 2rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 40rem;
+
+  & .title {
+    color: rgba(118, 191, 48, 0.9);
+    margin: 2rem;
+    border-bottom: 0.2rem solid rgba(118, 191, 48, 0.9);
+  }
+
+  & .sectionOneWrapper {
+    display: flex;
+    width: 85%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+  }
+
+  & .col {
+    display: flex;
+    padding: 1rem 2rem;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0 0.7rem;
+    //border: 0.1rem solid rgba(51, 51, 51, .5);
+    box-shadow: rgba(100, 100, 111, 0.3) 0px 7px 29px 0px;
+  }
+
+  & .col1,
+  .col2,
+  .col3 {
+    width: 100%;
+    padding: 1rem;
+  }
+
+  & .cardOne,
+  .cardFour {
+    border-top: 0.4rem solid rgba(118, 191, 48, 0.9);
+  }
+
+  & .cardTwo,
+  .cardThree {
+    border-top: 0.4rem solid rgba(0, 0, 0, 0.9);
+  }
+
+  & .col,
+  .cardTwo {
+    margin-bottom: 2rem;
+  }
+
+  & .col2 {
+    justify-content: space-between;
+  }
+
+  & .cardOne h3,
+  .cardFour h3 {
+    font-weight: 500;
+    color: rgba(0, 0, 0, 0.9);
+  }
+
+  & .cardTwo h3,
+  .cardThree h3 {
+    font-weight: 500;
+    color: rgba(118, 191, 48, 0.9);
+  }
+
+  & .col p {
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 0.9rem;
+  }
+
+  & .icons {
+    width: 4rem;
+    height: 4rem;
+    margin: 1rem;
+  }
+
+  & svg {
+    width: 100%;
+    height: 100%;
+  }
+
+  & .icons img {
+    width: 100%;
+    height: 100%;
+  }
+
+  ${media.mobile} {
+    flex-direction: column;
+
+    & .title {
+      margin: 1rem;
+    }
+
+    & .sectionOneWrapper {
+      flex-direction: column;
+      width: 100%;
+    }
+
+    & .col {
+      margin: 0.7rem 0.7rem;
+      box-shadow: rgba(100, 100, 111, 0.3) 0px 7px 29px 0px;
+    }
+
+    & .col p {
+    }
+
+    & .icons {
+      width: 4rem;
+      height: 4rem;
+      margin: 1rem;
+    }
+  }
+
+  ${media.tablet} {
+    padding: 0 1rem;
+    height: 25rem;
+
+    & .title {
+      margin: 0 0 1.5rem 0;
+    }
+
+    & .sectionOneWrapper {
+    }
+
+    & .col {
+      padding: 0.1rem 0.3rem;
+      margin: 0 0.2rem;
+      border: #333;
+    }
+
+    & .col h3 {
+      font-weight: 500;
+      font-size: 1.2rem;
+      color: rgba(118, 191, 48, 0.9);
+    }
+
+    & .col p {
+      font-size: 0.85rem;
+    }
+
+    & .icons {
+      width: 3rem;
+      height: 3rem;
+      margin: 1rem;
+    }
+  }
+
+  ${media.desktop} {
+    height: 30rem;
+  }
+
+  ${media.extralarge} {
+    height: 50rem;
+  }
+`;
 
 const ContactCards = () => {
     return (
-        <div>
-            
-        </div>
-    )
+      <div>
+        <Main>
+          <h3 className="title">Contact Us</h3>
+          <div className="sectionOneWrapper">
+            <div className="col1">
+              <div className="col cardOne">
+                <div className="icons">
+                  <img src="/icons/transparency.png" />
+                </div>
+                <h3>Transparency</h3>
+                <p>
+                  Unik Sacco is one of the accredited saccos found within
+                  Nairobi that has been established under strict adherance to
+                  the Kenyan Laws.Save with us, get loans and grow with us.
+                </p>
+              </div>
+            </div>
+            <div className="col2">
+              <div className="col cardTwo">
+                <div className="icons">
+                  <img src="/icons/clipboard.png" />
+                </div>
+                <h3>Accountability</h3>
+                <p>
+                  Unik Sacco is one of the accredited saccos found within
+                  Nairobi that has been established under strict adherance to
+                  the Kenyan Laws.Save with us, get loans and grow with us.
+                </p>
+              </div>
+
+              <div className="col cardThree">
+                <div className="icons">
+                  <img src="/icons/graph.png" />
+                </div>
+                <h3>Growth</h3>
+                <p>
+                  Unik Sacco is one of the accredited saccos found within
+                  Nairobi that has been established under strict adherance to
+                  the Kenyan Laws.Save with us, get loans and grow with us.
+                </p>
+              </div>
+            </div>
+            <div className="col3">
+              <div className="col cardFour">
+                <div className="icons">
+                  <img src="/icons/graph.png" />
+                </div>
+                <h3>Growth</h3>
+                <p>
+                  Unik Sacco is one of the accredited saccos found within
+                  Nairobi that has been established under strict adherance to
+                  the Kenyan Laws.Save with us, get loans and grow with us.
+                </p>
+              </div>
+            </div>
+          </div>
+        </Main>
+      </div>
+    );
 }
 
 export default ContactCards
